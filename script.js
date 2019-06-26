a48f21693a4f866308ac3df853b31e714aa522af
handaike-ta <50145556+handaike-ta@users.noreply.github.com>
2019-06-26 22:57:52 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+$(function() {
+  $('.vegas').vegas({
+    slides: [
+     { src: 'http://pas-pol.jp/wp-content/uploads/2014/11/main_visual_13-0x0.jpg' },
+     { src: 'http://pas-pol.jp/wp-content/uploads/2014/11/main_visual_2-0x0.jpg' },
+     { src: 'http://pas-pol.jp/wp-content/uploads/2014/11/main_visual_6-0x0.jpg' },
+     { src: 'http://pas-pol.jp/wp-content/uploads/2014/11/main_visual_7-0x0.jpg' },
+     { src: 'http://pas-pol.jp/wp-content/uploads/2014/11/main_visual_111-0x0.jpg' }
+    ],
+     delay: 7000,
+     timer: false,
+     transitionDuration: 500
+  });
+  $('.circle').click(function(){
+    $('html,body').animate({
+      'scrollTop': 0
+    },500);
+  });
+  $('.menu p i').click(function(){
+    if($('.menu p i').hasClass('active')){
+    $('.menu p i').removeClass('active');
+    $('.header-two ul').slideUp();
+  }else{
+    $('.menu p i').addClass('active');
+    $('.header-two ul').slideDown();
+  }
+  });
+});
